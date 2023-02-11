5776ade37ea1942b7076fd81e6069cb670f089b8
samarmeena <sumarmeena@gmail.com>
2023-02-11 14:45:48 +0530

    feat: ytdl-player-plugin

---
+++ plugins/ytdl-player/test/main.ts
@@ -0,0 +1,48 @@
+import { YTDLPlayerPlugin } from "@discordx/plugin-ytdl-player";
+import { IntentsBitField } from "discord.js";
+import { Client, MetadataStorage } from "discordx";
+
+const ytdlPlayerPlugin = new YTDLPlayerPlugin({
+  metadata: MetadataStorage.instance,
+});
+
+export class Main {
+  private static _client: Client;
+
+  static get Client(): Client {
+    return this._client;
+  }
+
+  static async start(): Promise<void> {
+    this._client = new Client({
+      // botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)],
+      intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessageReactions,
+        IntentsBitField.Flags.GuildVoiceStates,
+      ],
+      plugins: [ytdlPlayerPlugin],
+      silent: false,
+    });
+
+    this._client.once("ready", async () => {
+      await this._client.initApplicationCommands();
+
+      console.log("Bot started");
+    });
+
+    this._client.on("interactionCreate", (interaction) => {
+      this._client.executeInteraction(interaction);
+    });
+
+    // let's start the bot
+    if (!process.env.BOT_TOKEN) {
+      throw Error("Could not find BOT_TOKEN in your environment");
+    }
+    await this._client.login(process.env.BOT_TOKEN);
+  }
+}
+
+Main.start();
